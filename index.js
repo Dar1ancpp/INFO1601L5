3f9d5788e403a8f9d52e2c6cc5679bed43def5fc
Dar1ancpp <darian.php@gmail.com>
2025-03-07 00:54:14 -0400

    Task 3

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
+let name = "bob";
+let age = 24;
 
-for(let i=0; i< arr.length; i++){
-  console.log(arr[i])
-}
+console.log(typeof(name))//string
+console.log(typeof(age))//number
 
-let radius = 7;
-const pi = 3.14;
+console.log(`Hello my name is ${name}, I'm ${age} years old`);
+//Hello my name is bob, I'm 24 years old.
 
-let area = radius * radius * pi;
-
-
+console.log(`I was born in ${2020 - age}`)
+//I was born in 1966
 
 
 
